a693491712a8eb1e100df768238cc40bdd7780be
回忆旋涡 <516122314@qq.com>
2020-12-25 18:09:54 +0800

    MASTER

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -15,7 +15,7 @@
 
     <!--首页导航-->
     <nav class="msite_nav border-1px">
-      <div class="swiper-container">
+      <div class="swiper-container" v-if="categorys.length">
         <div class="swiper-wrapper">
           <div class="swiper-slide" v-for="(categorys,index) in categorysArr" :key="index">
             <a href="javascript:" class="link_to_food" v-for="(category,index) in categorys" :key="index">
@@ -29,6 +29,7 @@
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
       </div>
+      <img src="./images/msite_back.svg" v-else>
     </nav>
     <!--首页附近商家-->
     <div class="msite_shop_list border-1px">
